301d41f79b8d667841ccbf9e37349f033ba7966c
Sebastian Gebski <sebastian@gebski.net>
2016-10-25 13:16:29 +0200

    Dockerfiles fixed

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule GossipPhx.Mixfile do
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
+     {:phoenix_live_reload, "~> 1.0"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:exrm, "~> 1.0"}]

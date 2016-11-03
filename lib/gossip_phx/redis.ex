9e0afd5b8d656c2f0d4bbe35ffd983017e4400fe
Sebastian Gebski <sebastian@gebski.net>
2016-11-03 16:34:35 +0100

    Redis tests fixed

--- lib/gossip_phx/redis.ex
+++ lib/gossip_phx/redis.ex
@@ -10,12 +10,10 @@ defmodule GossipPhx.RedisApi do
 
   def init(nil) do
     {:ok, client} = Exredis.start_link
-    Logger.debug "Initializing Redis client"
     {:ok, %{ client: client }}
   end
 
   def terminate(_, state) do
-    Logger.debug "Terminating Redis client"
     state
     |> Map.get(:client)
     |> Exredis.stop
@@ -29,11 +27,14 @@ defmodule GossipPhx.RedisApi do
     GenServer.call(:redis, {:get_whispers})
   end
 
+  def clean_whispers() do
+    GenServer.call(:redis, {:clean_whispers})
+  end
+
   def handle_call({:add_whisper, content}, _from, state) do
     new_whisper =
       content
       |> Map.get("content")
-    Logger.debug "Adding content: #{inspect(new_whisper)}"
     client =
       state
       |> Map.get(:client)
@@ -45,19 +46,22 @@ defmodule GossipPhx.RedisApi do
   end
 
   def handle_call({:get_whispers}, _from, state) do
-    Logger.debug "Gonna return whispers!"
-    Logger.debug "State: #{inspect(state)}"
-    Logger.debug "Client: #{inspect(state |> Map.get(:client))}"
     unmapped =
       state
       |> Map.get(:client)
       |> Exredis.Api.lrange("whispers", 0, 10)
-    Logger.debug "Returning unmapped! #{Enum.count(unmapped)}"
     whispers =
       unmapped
       |> Enum.map(fn x -> whisper(content: x) end)
-    Logger.debug "Returning whispers: #{Enum.count(whispers)}"
-    Logger.debug "First whisper: #{List.first(unmapped)}"
     {:reply, whispers, state}
+  end
+
+  def handle_call({:clean_whispers}, _from, state) do
+    client =
+      state
+      |> Map.get(:client)
+    client
+    |> Exredis.Api.ltrim("whispers", 100, 101)
+    {:reply, :ok, state}
   end
 end
--- test/controllers/whisper_controller_test.exs
+++ test/controllers/whisper_controller_test.exs
@@ -1,9 +1,13 @@
 defmodule GossipPhx.WhisperControllerTest do
   use GossipPhx.ConnCase
+  import GossipPhx.Whisper
 
-  alias GossipPhx.Whisper
   @valid_attrs %{content: "some content"}
   @invalid_attrs %{}
+
+  setup do
+    GossipPhx.RedisApi.clean_whispers()
+  end
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, whisper_path(conn, :index)
@@ -11,7 +15,10 @@ defmodule GossipPhx.WhisperControllerTest do
   end
 
   test "creates resource and displays it on the list", %{conn: conn} do
-    conn = post conn, whisper_path(conn, :create), new_whisper: @valid_attrs
-    # assert Repo.get_by(Whisper, @valid_attrs)
+    assert GossipPhx.RedisApi.get_whispers == []
+    post conn, whisper_path(conn, :create), new_whisper: @valid_attrs
+    whispers = GossipPhx.RedisApi.get_whispers
+    assert Enum.count(whispers) == 1
+    assert whisper(List.first(whispers), :content) == Map.get(@valid_attrs, :content)
   end
 end

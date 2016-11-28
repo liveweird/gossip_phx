825accc8b69425ace01ab9dd404348b105c7e19c
Sebastian Gebski <sebastian@gebski.net>
2016-11-28 16:40:28 +0100

    Just a bad day... Another issue with app name ...

--- lib/release/tasks.ex
+++ lib/release/tasks.ex
@@ -2,7 +2,7 @@ defmodule Release.Tasks do
   def migrate do
     {:ok, _} = Application.ensure_all_started(:gossip_phx)
     path = Application.app_dir(:gossip_phx, "priv/repo/migrations")
-    Ecto.Migrator.run(MyApp.Repo, path, :up, all: true)
+    Ecto.Migrator.run(GossipPhx.Repo, path, :up, all: true)
     :init.stop()
   end
 end

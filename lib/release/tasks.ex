52662c44e161706f90ec6bcb25b71ae8c7f7b9a2
Sebastian Gebski <sebastian@gebski.net>
2016-11-28 16:00:37 +0100

    Stupid mistake: I haven't change app name atom

--- lib/release/tasks.ex
+++ lib/release/tasks.ex
@@ -1,7 +1,7 @@
 defmodule Release.Tasks do
   def migrate do
-    {:ok, _} = Application.ensure_all_started(:my_app)
-    path = Application.app_dir(:my_app, "priv/repo/migrations")
+    {:ok, _} = Application.ensure_all_started(:gossip_phx)
+    path = Application.app_dir(:gossip_phx, "priv/repo/migrations")
     Ecto.Migrator.run(MyApp.Repo, path, :up, all: true)
     :init.stop()
   end

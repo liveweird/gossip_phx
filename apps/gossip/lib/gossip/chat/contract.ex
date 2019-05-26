3c63dc4b396a625dd0adbce72fb1ada9af2dcd7b
Sebastian Gebski <sebastian@gebski.net>
2019-05-26 16:38:26 +0200

    Basic channel tests

--- apps/gossip/lib/gossip/chat/contract.ex
+++ apps/gossip/lib/gossip/chat/contract.ex
@@ -5,5 +5,6 @@ defmodule Chat.Contract do
   defdelegate get_channel(channel_name), to: Chat.Service
   defdelegate join_channel(channel_name, user_name), to: Chat.Service
   # defdelegate leave_channel(channel_name, user_name), to: Chat.Service
+  # defdelegate get_all_users_in_channel(channel_name), to: Chat.Service
 
 end
--- apps/gossip/test/chat/chat_test.exs
+++ apps/gossip/test/chat/chat_test.exs
@@ -8,5 +8,36 @@ defmodule Chat.ChannelTest do
 
       assert length(channels) == 0
     end
+
+    test "create a channel" do
+      {:ok, inserted} = Chat.Contract.create_channel("xyz")
+
+      assert inserted.name == "xyz"
+      assert inserted.is_private == false
+      assert inserted.is_deleted == false
+    end
+
+    test "field-level validation prevents channel from being created" do
+      {:error, message} = Chat.Contract.create_channel(nil)
+
+      assert "Channel has not been created." == message
+    end
+
+    test "duplication prevents channel from being created" do
+      {:ok, inserted} = Chat.Contract.create_channel("xyz")
+      {:error, message} = Chat.Contract.create_channel("xyz")
+
+      assert "Channel has not been created." == message
+    end
+
+    test "created user is the same as received" do
+      {:ok, inserted} = Chat.Contract.create_channel("xyz")
+      {:ok, retrieved} = Chat.Contract.get_channel("xyz")
+
+      assert inserted.name == retrieved.name
+      assert inserted.is_private == retrieved.is_private
+      assert inserted.is_deleted == retrieved.is_deleted
+    end
+
   end
 end

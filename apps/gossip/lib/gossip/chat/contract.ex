a464e16e02c23aa870565c3b33e3fedcf95f8293
Sebastian Gebski <sebastian@gebski.net>
2019-06-19 23:22:42 +0200

    Missing typespecs

--- apps/gossip/lib/gossip/chat/contract.ex
+++ apps/gossip/lib/gossip/chat/contract.ex
@@ -15,7 +15,9 @@ defmodule Chat.Contract do
   @spec leave_channel(String.t(), String.t()) :: {:ok, %Chat.Channel{}} | {:error, String.t()}
   defdelegate leave_channel(channel_name, user_name), to: Chat.Service
 
+  @spec is_user_in_channel(String.t(), String.t()) :: true | false
   defdelegate is_user_in_channel(channel_name, user_name), to: Chat.Service
-  defdelegate get_all_users_in_channel(channel_name), to: Chat.Service
 
+  @spec get_all_users_in_channel(String.t()) :: list(%People.User{})
+  defdelegate get_all_users_in_channel(channel_name), to: Chat.Service
 end
